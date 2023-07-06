e3ee27964ea9d56ea36c03d105f7071f560f49df
Sayf Roumane <sayfroumane@MBP-de-Sayf.lan>
2023-07-07 02:49:38 +0300

    add metro config using rnx kit resolving symlinks and running the app

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,11 +1,20 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
+const {makeMetroConfig} = require('@rnx-kit/metro-config');
+const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+const extraNodeModules = {
+  modules: path.resolve(path.join(__dirname, '../../node_modules')),
+};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const watchFolders = [path.resolve(path.join(__dirname, '../../node_modules'))];
+
+const nodeModulesPaths = [path.resolve(path.join(__dirname, './node_modules'))];
+
+module.exports = makeMetroConfig({
+  resolver: {
+    resolveRequest: MetroSymlinksResolver(),
+    extraNodeModules,
+    nodeModulesPaths,
+  },
+  watchFolders,
+});
